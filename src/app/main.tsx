be4f9ee72cc45953099dc03eaa4e9d083452ba29
diandev <dianhuiza@gmail.com>
2025-04-14 16:16:02 -0400

    feat: integrate Redux store and update main application structure

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { routes } from './routes.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from "../store/index.ts";
+import { Provider } from "react-redux";
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,12 +15,14 @@ const queryClient = new QueryClient({
       retry: false,
     },
   },
-})
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>  
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
