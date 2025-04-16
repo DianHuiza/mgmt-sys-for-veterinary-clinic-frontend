cc074531bcc2c6c67727b60b5b0bf20f2d572ac5
diandev <dianhuiza@gmail.com>
2025-04-16 17:24:10 -0400

    feat: update apiClient baseURL to use environment variable for backend URL

--- src/core/api/apiClient.ts
+++ src/core/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://api.example.com', // Cambia esto por tu URL base
+  baseURL: import.meta.env.BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
